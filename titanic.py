1646fa0ca09a4f3771210805b7e945ea00e65a7c
gindeleo <schindler3333@gmail.com>
2016-03-09 11:47:43 +0000

    setting up reading of data

--- titanic.py
+++ titanic.py
@@ -7,3 +7,13 @@
 
 #	In this challenge, we ask you to complete the analysis of what sorts of people were likely to survive. 
 #	In particular, we ask you to apply the tools of machine learning to predict which passengers survived the tragedy.
+
+
+import numpy as np 
+import pandas as pd 
+import matplotlib as plt
+import seaborn as sb 
+
+
+#read data from svn files
+
