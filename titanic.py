3d8eaf6e6d6fd638857ee3a9847f0221646aec23
gindeleo <schindler3333@gmail.com>
2016-03-09 11:27:55 +0000

    stub titanic.py file

---
+++ titanic.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+#Machine Learning test 
+#from https://www.kaggle.com/c/titanic
+
+#   **** TASK ****
+
+#	In this challenge, we ask you to complete the analysis of what sorts of people were likely to survive. 
+#	In particular, we ask you to apply the tools of machine learning to predict which passengers survived the tragedy.
