445052233a327a14eba299eb446a9ae4dc2978e9
Gregor <gregor.hannappel@gmail.com>
2016-03-09 15:42:20 +0000

    Update titanic.py

--- titanic.py
+++ titanic.py
@@ -1,21 +1,55 @@
 #!/usr/bin/python
 
-#Machine Learning test 
+#Machine Learning test
 #from https://www.kaggle.com/c/titanic
 
 #   **** TASK ****
 
-#	In this challenge, we ask you to complete the analysis of what sorts of people were likely to survive. 
+#	In this challenge, we ask you to complete the analysis of what sorts of people were likely to survive.
 #	In particular, we ask you to apply the tools of machine learning to predict which passengers survived the tragedy.
 
-import numpy as np 
-import pandas as pd 
-
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
 #read data from csv files
-test_df = pd.read_csv('test.csv')
-train_df = pd.read_csv('train.csv')
+test = pd.read_csv('test.csv')
+train = pd.read_csv('train.csv')
 
-print test_df.head(10)
-print test_df.describe()
+#print out info on train set
+# print train.head(10)
+# print train.describe()
+
+#clean up data
+
+# Some plots to see if the median age is the correct value to pic
+# plt.title("median = " + str(test["Age"].median()) + " mean = " + str(test["Age"].mean()))
+# plt.xlabel('Age')
+# plt.ylabel('frequency')
+# test["Age"].plot(kind="hist", bins=16)
+# plt.show()
+
+test["Age"] = test["Age"].fillna(test["Age"].median()) #fill missing values with median value
+train["Age"] = train["Age"].fillna(train["Age"].median())
+
+# plt.title("median = " + str(test["Age"].median()) + " mean = " + str(test["Age"].mean()))
+# plt.xlabel('Age')
+# plt.ylabel('frequency')
+# test["Age"].plot(kind="hist", bins=16)
+# plt.show()
+
+test["Embarked"]=test["Embarked"].fillna("S")
+train["Embarked"]=train["Embarked"].fillna("S")
+
+#make entries numerical for Sex and Embarked
+test.loc[test["Sex"] == "male", "Sex"] = 0 ; test.loc[test["Sex"] == "female", "Sex"] = 1
+train.loc[train["Sex"] == "male", "Sex"] = 0 ; train.loc[train["Sex"] == "female", "Sex"] = 1
+
+test.loc[test["Embarked"] == "S", "Embarked"] = 0
+test.loc[test["Embarked"] == "C", "Embarked"] = 1
+test.loc[test["Embarked"] == "Q", "Embarked"] = 2
+
+train.loc[train["Embarked"] == "S", "Embarked"] = 0
+train.loc[train["Embarked"] == "C", "Embarked"] = 1
+train.loc[train["Embarked"] == "Q", "Embarked"] = 2
 
